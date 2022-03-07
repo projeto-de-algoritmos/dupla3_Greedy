f58d497545df9b575c95215c2355f1bb419c27f4
sofiapatrocinio <sofiapatrocinio@hotmail.com>
2022-03-07 17:38:15 -0300

    Add time picker

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import paw from './assets/paw.png';
 import './App.css';
+import React, { useState } from 'react';
+import TimePicker from 'react-time-picker';
 
 function App() {
+  const [showTimePicker, setShowTimePicker] = useState(false);
+  const today = new Date();
+  const date = today.getDate() + '/' + (today.getMonth() + 1) + '/' + today.getFullYear();
+  const [hourValue, onChange] = useState('10:00');
+
+
   return (
     <>
       <div>
-          <img className="logo" src={paw} width="200"></img>
+        <img className="logo" src={paw} width="200"></img>
         <div className="cover">
           <h1 className="cover-title">Greedy</h1>
           <span className="cover-text">
@@ -13,11 +21,27 @@ function App() {
           </span>
         </div>
         <div className="action">
-          <button className="graph-button" type="button" onClick={() => { }}>
+          <button className="graph-button" type="button" onClick={() => { setShowTimePicker(true) }}>
             Agendar
           </button>
         </div>
       </div>
+      {showTimePicker ?
+        <div className="content">
+          <div>
+          <span className="cover-text">Agende seu horário para hoje, dia {date}</span>
+          </div>
+          <div className="picker">
+          <TimePicker onChange={onChange} value={hourValue} />
+          </div>
+          <div className="picker">
+          <button className="graph-button" type="button" onClick={() => { setShowTimePicker(true) }}>
+            Concluir
+          </button>
+          </div>
+        </div>
+        : null
+      }
     </>
   );
 }
